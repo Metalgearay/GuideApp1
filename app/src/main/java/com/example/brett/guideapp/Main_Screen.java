82cbe1b212db20b94760aa6ce5bfdc7c72edcef8
Brett <asmcollege12@hotmail.com>
2015-02-07 04:42:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/brett/guideapp/Main_Screen.java
@@ -0,0 +1,21 @@
+package com.example.brett.guideapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Main_Screen extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__screen);
+
+    }
+
+
+}
